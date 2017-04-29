027167735af5eef422792a3c6c0be37f58e1e17a
rtaveras824 <rtaveras824@hotmail.com>
2017-04-29 14:17:30 -0400

    src folder deleted to start over, simple react app

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Create a new component. This component should produce
+// some HTML
+const App = function() {
+	return (
+		<div>Hi!!!</div>
+	);
+}
+
+React.render(App);
