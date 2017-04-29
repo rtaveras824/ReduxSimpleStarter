64c16f8c02dcdb40f0d42053a838fc1494523810
rtaveras824 <rtaveras824@hotmail.com>
2017-04-29 14:49:18 -0400

    Search bar component changed from stateless functional component to class to handle onChange event

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,11 +1,16 @@
 // Even though react isn't called, we still need to import it
 // because webpack will turn <input /> to React.createElement
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-	return (
-		<input />
-	)
+class SearchBar extends Component {
+	// Every class must have a render method
+	render() {
+		return <input onChange={ this.onInputChange } />
+	}
+
+	onInputChange(event) {
+		console.log(event.target.value);
+	}
 };
 
 export default SearchBar;
