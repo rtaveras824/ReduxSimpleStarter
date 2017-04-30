998829307f16ae8f476f887501358c63217fa207
rtaveras824 <rtaveras824@hotmail.com>
2017-04-30 16:52:53 -0400

    Lodash debounce added to delay search input results

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -13,14 +13,14 @@ class SearchBar extends Component {
 	render() {
 		return (
 			<div className="search-bar">
-				<input value={ this.state.term } onChange={ this.onInputChange } />
+				<input value={ this.state.term } onChange={ (event) => this.onInputChange(event.target.value) } />
 			</div>
 		)
 	}
 
-	onInputChange(event) {
-		this.setState({ term: event.target.value });
-		console.log(this.state.term, event.target.value);
+	onInputChange(term) {
+		this.setState({ term });
+		this.props.onSearchTermChange(term);
 	}
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -19,7 +20,11 @@ class App extends Component {
 			selectedVideo: null
 		};
 
-		YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
+		this.videoSearch('surfboards');
+	}
+
+	videoSearch(term) {
+		YTSearch({ key: API_KEY, term: term}, (videos) => {
 			this.setState({ 
 				videos,
 				selectedVideo: videos[0]
@@ -28,14 +33,12 @@ class App extends Component {
 		});
 	}
 
-	videoSearch(term) {
-		
-	}
-
 	render() {
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
 		return (
 			<div>
-				<SearchBar />
+				<SearchBar onSearchTermChange={ videoSearch } />
 				<VideoDetail video={ this.state.selectedVideo } />
 				<VideoList 
 					onVideoSelect={ selectedVideo => this.setState({ selectedVideo })}
